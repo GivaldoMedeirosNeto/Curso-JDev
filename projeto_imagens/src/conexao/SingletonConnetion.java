a2a277f874735908af8196365bad1ca40172ddf9
Givaldo Medeiros Neto <givaldo.ctinf@outlook.com>
2023-09-28 22:10:58 -0300

    Commit IDE

---
+++ projeto_imagens/src/conexao/SingletonConnetion.java
@@ -0,0 +1,52 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Classe responsável por controlar , criar e retornar a conexão com o banco de dados
+ * @author alex
+ *
+ */
+public class SingletonConnetion {
+
+	//Conexão SQL Java
+	private static Connection connection = null;
+
+	// Método estático que faz sempre a chamada para conectar
+	static {
+		conectar();
+	}
+
+	public SingletonConnetion() {
+		conectar();
+	}
+
+	/**
+	 * Realiza a conexão com o banco de dados
+	 */
+	private static void conectar() {
+		try {
+			
+			if (connection == null) {
+				Class.forName("org.postgresql.Driver"); // driver mysql
+				connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/projeto_imagens", "postgres", "admin");// url do banco de dados com user e senha
+				connection.setAutoCommit(false);// não dar commit automatico
+				connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // tipo da transação no banco de dados
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao conectar com a base de dados."
+					+ e);
+		}
+
+	}
+
+	/**
+	 * Retorna a conexão com o banco de dados
+	 * @return Connection
+	 */
+	public static Connection getConnection() {
+		return connection;
+	}
+
+}
